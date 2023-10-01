da0f48a976c538a6d03eb6b307392e63bfcd1e31
Houstin Angwenyi <houstin.angwenyi@student.moringaschool.com>
2023-10-02 00:22:46 +0300

    corrected imports

--- server/api/app.py
+++ server/api/app.py
@@ -5,7 +5,7 @@ from flask_restful import Api
 from flask_migrate import Migrate
 
 
-from models import db
+from .models import db
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://restaurant_pizza_db_oanm_user:lfG9ZOfdDpqkOLJWDfZei7VzVS7GdOQk@dpg-ckctqlect0pc73c1b0l0-a.ohio-postgres.render.com/restaurant_pizza_db_oanm"
